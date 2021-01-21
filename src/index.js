8f89d76fea1e00310092111674e3803fcc22b139
AnirudhaRamteke <nganirudh2016.com>
2021-01-21 22:33:23 +0530

    Added questions from API

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './assets/style.css';
 import he from 'he';
 import QuestionBox from './components/QuestionBox';
 import Result from './components/Result';
-// import quizService from './quizService';
 
 
 
@@ -16,16 +15,10 @@ class Quiz extends Component {
   };
   
   getQuestions = () => {
-    // quizService().then(question => {
-    //   this.setState({
-    //     questionBank: question
-    //   });
-    // })
-
+ 
     fetch('https://opentdb.com/api.php?amount=10&category=19')
     .then(res => res.json())
     .then(result => {
-      console.log(result.results)
       this.setState({
         questionBank: result.results
       });
@@ -62,26 +55,6 @@ combine = (arr, ele) => {
   componentDidMount(){
     this.getQuestions();
   }
-//   render(){
-//     return(
-//       <div className="container">
-//       <div className="title">Quiz</div>
-//       {this.state.questionBank.length > 0 &&
-//         this.state.responses < 5 &&
-//          this.state.questionBank.map(({question, answers, correct, questionId}) => (
-//           <QuestionBox
-//                 question = {question}
-//                 options= {answers}
-//                 key = {questionId}
-//                 selected= {answer => this.computeAnswer(answer, correct)}
-//           />   )  
-//       )}
-
-//       {this.state.responses === 5 ? (<Result score={this.state.score} playAgain={this.playAgain}/>): null }
-//       </div>
-//     );
-//   }
-// }
 
 render(){
   return(
